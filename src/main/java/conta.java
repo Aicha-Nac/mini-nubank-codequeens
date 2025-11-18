cc04674f1d57cb6ff2a8a31ec98304d8ce972ce0
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-18 14:37:50 -0300

    Update conta.java

--- src/main/java/conta.java
+++ src/main/java/conta.java
@@ -1,1 +1,34 @@
+public abstract class Conta {
+    protected int numero;
+    protected double saldo;
+
+    public Conta(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() { return numero; }public abstract class Conta {
+    protected int numero;
+    protected double saldo;public abstract class Conta {
+    protected int numero;
+    protected double saldo;
+
+    public Conta(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() { return numero; }
+    public double getSaldo() { return saldo; }
+}
+
+
+    public Conta(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() { return numero; }
+    public double getSaldo() { return saldo; }
+}
+
+    public double getSaldo() { return saldo; }
+}
 
