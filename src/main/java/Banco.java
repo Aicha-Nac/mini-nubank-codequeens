c6a07069262636410b4e881a151846c605758daa
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-18 14:40:31 -0300

    Update Banco.java

--- src/main/java/Banco.java
+++ src/main/java/Banco.java
@@ -1,1 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Banco {
+
+    private Map<Integer, Conta> contas = new HashMap<>();
+    private int proximoNumero = 1;
+
+    public Conta criarConta() {
+        Conta c = new ContaCorrente(proximoNumero++);
+        contas.put(c.getNumero(), c);
+        return c;
+    }
+}
 
