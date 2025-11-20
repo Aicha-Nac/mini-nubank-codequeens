c59785609c68349c26a5cfb0fd9c51195ea6a23f
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-20 00:21:46 -0300

    Update Conta.java

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -1,1 +1,17 @@
+package model;
+
+import java.io.Serializable;
+
+public abstract class Conta implements Transacionavel, Serializable {
+    protected Cliente cliente;
+    protected double saldo;
+
+    public Conta(Cliente cliente) {
+        this.cliente = cliente;
+        this.saldo = 0.0;
+    }
+
+    public double getSaldo() { return saldo; }
+    public Cliente getCliente() { return cliente; }
+}
 
