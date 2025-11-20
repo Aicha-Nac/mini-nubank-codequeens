b1bdbbff2afea5f06a10875ea2d611a32805fcdc
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-20 00:29:20 -0300

    Update Persistencia.java

--- src/service/Persistencia.java
+++ src/service/Persistencia.java
@@ -1,1 +1,21 @@
+package service;
+
+import java.io.*;
+import java.util.Map;
+
+public class Persistencia {
+
+    public static void salvar(Object obj, String caminho) throws Exception {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(caminho));
+        out.writeObject(obj);
+        out.close();
+    }
+
+    public static Object carregar(String caminho) throws Exception {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(caminho));
+        Object obj = in.readObject();
+        in.close();
+        return obj;
+    }
+}
 
