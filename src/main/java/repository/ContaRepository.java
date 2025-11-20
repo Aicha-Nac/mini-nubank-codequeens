a005a0d6fdd9661eb83432150aeed0311ad04b27
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-19 22:34:10 -0300

    Update ContaRepository.java

--- src/main/java/repository/ContaRepository.java
+++ src/main/java/repository/ContaRepository.java
@@ -1,1 +1,35 @@
+package repository;
+
+import model.Conta;
+import java.util.*;
+
+public class ContaRepository {
+
+    private List<Conta> contas = new ArrayList<>();
+
+    // Salvar conta
+    public void salvar(Conta conta) {
+        contas.add(conta);
+    }
+
+    // Listar todas
+    public List<Conta> listarTodas() {
+        return contas;
+    }
+
+    // Buscar conta pelo número
+    public Conta buscarPorNumero(String numero) {
+        for (Conta c : contas) {
+            if (c.getNumero().equals(numero)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    // Verificar se número já existe
+    public boolean numeroExiste(String numero) {
+        return buscarPorNumero(numero) != null;
+    }
+}
 
