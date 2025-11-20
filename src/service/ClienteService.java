7eb843ee63a858772e81352a93c0aa5d5cc19910
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-20 00:23:34 -0300

    Update ClienteService.java

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -1,1 +1,15 @@
+package service;
+
+import model.Cliente;
+import java.util.*;
+
+public class ClienteService {
+    private Map<String, Cliente> clientes = new HashMap<>();
+
+    public Cliente cadastrar(String nome, String cpf) {
+        Cliente c = new Cliente(nome, cpf);
+        clientes.put(cpf, c);
+        return c;
+    }
+}
 
