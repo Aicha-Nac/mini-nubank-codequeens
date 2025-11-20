ec1e24ed3d9e1ee69449fcde2aed5fc5dff8439b
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-19 23:09:49 -0300

    Update Main.java

--- src/main/java/data/docs/src/main/java/Main.java
+++ src/main/java/data/docs/src/main/java/Main.java
@@ -1,7 +1,4 @@
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Aplicativo Nubank iniciado!");
     import java.util.Scanner;
 
 import model.Cliente;
