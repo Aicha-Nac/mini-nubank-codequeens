c7c0cb4624f35bea8d8ded893c55741c9c542046
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-20 00:12:37 -0300

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,1 +1,9 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Projeto Mini-Nubank iniciado com sucesso!");
+        System.out.println("Estrutura do projeto funcionando corretamente.");
+    }
+}
 
