0857149c6117311253852cfff65c7df4677caf55
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-20 00:24:01 -0300

    Update ContaService.java

--- src/service/ContaService.java
+++ src/service/ContaService.java
@@ -1,1 +1,15 @@
+package service;
+
+import model.*;
+import java.util.*;
+
+public class ContaService {
+    private Map<String, Conta> contas = new HashMap<>();
+
+    public Conta criarConta(Cliente cliente) {
+        Conta nova = new ContaCorrente(cliente);
+        contas.put(cliente.getCpf(), nova);
+        return nova;
+    }
+}
 
