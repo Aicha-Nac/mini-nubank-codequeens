826b09fd70a4e5b3b3c13d31fcb21df2f161ba6d
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:39:51 -0300

    Update AuthService.java

--- src/main/java/service/AuthService.java
+++ src/main/java/service/AuthService.java
@@ -1,1 +1,26 @@
+package service;
+
+import pessoas.Cliente;
+import repository.ClienteRepository;
+
+public class AuthService {
+
+    private ClienteRepository clienteRepository;
+
+    public AuthService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente login(String cpf) {
+        Cliente cliente = clienteRepository.buscarPorCpf(cpf);
+
+        if (cliente != null) {
+            System.out.println("Login realizado com sucesso!");
+            return cliente;
+        } else {
+            System.out.println("CPF não encontrado. Login falhou.");
+            return null;
+        }
+    }
+}
 
