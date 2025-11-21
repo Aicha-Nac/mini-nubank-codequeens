2701872104a513e45337cc37d1b4f76926f731bc
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:46:30 -0300

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,20 +12,18 @@ public class Main {
         ClienteRepository clienteRepository = new ClienteRepository();
         AuthService auth = new AuthService(clienteRepository);
 
+        // cria cliente novo e salva
         Cliente c1 = new Cliente("Aïcha", "123.456.789-00");
         clienteRepository.salvar(c1);
 
-        Cliente logado = auth.login("123.456.789-00"); // login correto
+        // faz login (deve funcionar mesmo após fechar o programa)
+        Cliente logado = auth.login("123.456.789-00");
 
         if (logado != null) {
             ContaCorrente cc = new ContaCorrente(logado);
-            cc.depositar(100);
-            System.out.println("Saldo após depósito: " + cc.getSaldo());
+            cc.depositar(200);
+            System.out.println("Saldo: " + cc.getSaldo());
         }
-
-        // login errado
-        auth.login("000.000.000-00");
     }
 }
-
 
