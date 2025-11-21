60db84c0133757ac17bd6f3730a6e8b992bcd5ee
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:25:22 -0300

    Update Cliente.java

--- src/main/java/pessoas/Cliente.java
+++ src/main/java/pessoas/Cliente.java
@@ -1,1 +1,15 @@
+package pessoas;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() { return nome; }
+    public String getCpf() { return cpf; }
+}
 
