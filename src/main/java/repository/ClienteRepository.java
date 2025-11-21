e1cb3a389f449864b124fe1fbac540697daae95b
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:40:52 -0300

    Update ClienteRepository.java

--- src/main/java/repository/ClienteRepository.java
+++ src/main/java/repository/ClienteRepository.java
@@ -1,36 +1,24 @@
 package repository;
 
-import model.Cliente;
-import java.util.*;
+import pessoas.Cliente;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteRepository {
 
-    // Lista que armazena todos os clientes
     private List<Cliente> clientes = new ArrayList<>();
 
-    // Adiciona um cliente
     public void salvar(Cliente cliente) {
         clientes.add(cliente);
     }
 
-    // Retorna todos os clientes
-    public List<Cliente> listarTodos() {
-        return clientes;
-    }
-
-    // Busca cliente pelo CPF
     public Cliente buscarPorCpf(String cpf) {
         for (Cliente c : clientes) {
             if (c.getCpf().equals(cpf)) {
                 return c;
             }
         }
-        return null; // não achou
-    }
-
-    // Verifica se já existe cliente com CPF X
-    public boolean cpfExiste(String cpf) {
-        return buscarPorCpf(cpf) != null;
+        return null;
     }
 }
 
