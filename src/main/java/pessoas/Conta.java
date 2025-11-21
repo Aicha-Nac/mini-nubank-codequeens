f118e127815a93d6fb210772cc7b91fde5b4a095
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:26:21 -0300

    Update Conta.java

--- src/main/java/pessoas/Conta.java
+++ src/main/java/pessoas/Conta.java
@@ -1,1 +1,29 @@
+package contas;
+
+import pessoas.Cliente;
+
+public abstract class Conta {
+    protected Cliente cliente;
+    protected double saldo;
+
+    public Conta(Cliente cliente) {
+        this.cliente = cliente;
+        this.saldo = 0;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public double getSaldo() { return saldo; }
+    public Cliente getCliente() { return cliente; }
+}
 
