360ea63452c29cb77635b48b35bc56c32ce3f5b0
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:28:32 -0300

    Create Persistencia.java

---
+++ src/main/java/service/Persistencia.java
@@ -0,0 +1,14 @@
+package service;
+
+import banco.Banco;
+
+public class Persistencia {
+
+    public static void salvar(Banco banco) {
+        System.out.println("Salvando dados... (simulação)");
+    }
+
+    public static void carregar(Banco banco) {
+        System.out.println("Carregando dados... (simulação)");
+    }
+}
