2728b5c5f71d20bca98b1063118ce4e746be7d9f
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:27:16 -0300

    Create Banco.java

---
+++ src/main/java/banco/Banco.java
@@ -0,0 +1,17 @@
+package banco;
+
+import contas.Conta;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+    private List<Conta> contas = new ArrayList<>();
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+}
