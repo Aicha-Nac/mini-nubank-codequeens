99dad462c0747d92b00f77378c69cb0f001c4a0c
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:34:17 -0300

    Update ContaPoupanca.java

--- src/main/java/contas/ContaPoupanca.java
+++ src/main/java/contas/ContaPoupanca.java
@@ -1,1 +1,16 @@
+package contas;
+
+import pessoas.Cliente;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    public void renderJuros() {
+        double juros = saldo * 0.003; // 0.3% ao mês (exemplo)
+        saldo += juros;
+    }
+}
 
