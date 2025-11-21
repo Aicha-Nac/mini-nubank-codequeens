4d1b23465c4c6decab0977cebab53b8c8518bf4e
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 00:34:46 -0300

    Update ContaCorrente.java

--- src/main/java/contas/ContaCorrente.java
+++ src/main/java/contas/ContaCorrente.java
@@ -1,1 +1,23 @@
+package contas;
+
+import pessoas.Cliente;
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        double taxa = 1.50; // taxa fixa
+        double total = valor + taxa;
+
+        if (total <= saldo) {
+            saldo -= total;
+            return true;
+        }
+        return false;
+    }
+}
 
