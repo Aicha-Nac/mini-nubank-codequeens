fd0fa413dab8675f3a4b5473bafbf2d9660d0b17
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 23:53:46 -0300

    Update AuthService.java

--- src/service/AuthService.java
+++ src/service/AuthService.java
@@ -1,8 +1,25 @@
 package service;
 
+import model.Cliente;
+import repository.ClienteRepository;
+
 public class AuthService {
-    public boolean login(String cpf) {
-        return cpf != null && !cpf.isEmpty();
+
+    private ClienteRepository clienteRepo;
+
+    public AuthService(ClienteRepository clienteRepo) {
+        this.clienteRepo = clienteRepo;
+    }
+
+    public Cliente loginPorCpf(String cpf) {
+        Cliente c = clienteRepo.buscarPorCpf(cpf);
+        if (c == null) {
+            System.out.println("Cliente não encontrado para CPF: " + cpf);
+        } else {
+            System.out.println("Login bem sucedido: " + c.getNome());
+        }
+        return c;
     }
 }
+
 
