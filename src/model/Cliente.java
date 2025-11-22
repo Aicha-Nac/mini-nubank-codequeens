201093a56dcbddb9469c07d2a8f900095e0f7ca7
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 22:39:45 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,3 +1,71 @@
+package model;
+
+import java.util.Objects;
+
+public class Cliente {
+
+    private String id;
+    private String nome;
+    private String email;
+
+    public Cliente() {}
+
+    public Cliente(String id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    // Getters e Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // hashCode e equals (necessários para Set e Map)
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Cliente other = (Cliente) obj;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+
+
 private List<Conta> contas = new ArrayList<>();
 
 public void adicionarConta(Conta conta) {
