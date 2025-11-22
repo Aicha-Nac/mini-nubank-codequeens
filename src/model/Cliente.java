f4fab7b8d0cda5c649a7f95101a77f33e340665a
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 23:47:44 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,85 +1,72 @@
 package model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class Cliente {
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-    private String id;
+    private String id;     // usar CPF ou ID gerado
     private String nome;
+    private String cpf;
     private String email;
+    private transient List<Conta> contas = new ArrayList<>(); // transient: não precisamos serializar referências duplicadas dependendo da escolha
 
     public Cliente() {}
 
-    public Cliente(String id, String nome, String email) {
+    public Cliente(String id, String nome, String cpf, String email) {
         this.id = id;
         this.nome = nome;
+        this.cpf = cpf;
         this.email = email;
+        this.contas = new ArrayList<>();
     }
 
-    // Getters e Setters
-    public String getId() {
-        return id;
+    // getters / setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public void adicionarConta(Conta c) {
+        if (contas == null) contas = new ArrayList<>();
+        contas.add(c);
+    }
+    public List<Conta> getContas() {
+        if (contas == null) contas = new ArrayList<>();
+        return contas;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente)) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(cpf, cliente.cpf);
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    // hashCode e equals (necessários para Set e Map)
     @Override
     public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Cliente other = (Cliente) obj;
-        return Objects.equals(id, other.id);
+        return Objects.hash(cpf);
     }
 
     @Override
     public String toString() {
-        return "Cliente{" +
-                "id='" + id + '\'' +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+        return "Cliente{" + "id='" + id + '\'' + ", nome='" + nome + '\'' + ", cpf='" + cpf + '\'' + ", email='" + email + '\'' + '}';
     }
-
-
-
-private List<Conta> contas = new ArrayList<>();
-
-public void adicionarConta(Conta conta) {
-    contas.add(conta);
 }
 
-public List<Conta> getContas() {
-    return contas;
-import java.util.List;
-import java.util.ArrayList;
 
 
 
-}
+
+
 
 
 
