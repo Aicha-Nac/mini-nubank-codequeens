be132b7bde2962b914810ce9232099d70d6079c5
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 23:50:00 -0300

    Update ContaCorrente.java

--- src/model/ContaCorrente.java
+++ src/model/ContaCorrente.java
@@ -1,6 +1,33 @@
+package model;
+
 public class ContaCorrente extends Conta {
-    public ContaCorrente(int numero) {
-        super(numero);
+    private static final long serialVersionUID = 1L;
+
+    private double tarifaSaque = 1.50;
+
+    public ContaCorrente() { super(); }
+
+    public ContaCorrente(String agencia, String titularCpf) {
+        super(agencia, titularCpf);
+    }
+
+    @Override
+    public boolean depositar(double valor) {
+        if (valor <= 0) return false;
+        saldo += valor;
+        registrarTransacao(new Transacao("DEPOSITO", valor, "Depósito em conta corrente"));
+        return true;
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        double total = valor + tarifaSaque;
+        if (valor <= 0 || total > saldo) return false;
+        saldo -= total;
+        registrarTransacao(new Transacao("SAQUE", -valor, "Saque (tarifa aplicada)"));
+        return true;
     }
 }
+
+
 
