6b202e6dbf0dca10f338cc655827e32cdc10afb4
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 23:48:33 -0300

    Update Transacionavel.java

--- src/model/Transacionavel.java
+++ src/model/Transacionavel.java
@@ -1,5 +1,34 @@
-public interface Transacionavel {
-    boolean depositar(double valor);
-    boolean sacar(double valor);
+
+package model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Transacao implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String tipo; // DEPOSITO, SAQUE, TRANSFERENCIA, PIX
+    private double valor;
+    private LocalDateTime instante;
+    private String descricao;
+
+    public Transacao() {}
+
+    public Transacao(String tipo, double valor, String descricao) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.instante = LocalDateTime.now();
+        this.descricao = descricao;
+    }
+
+    public String getTipo() { return tipo; }
+    public double getValor() { return valor; }
+    public LocalDateTime getInstante() { return instante; }
+    public String getDescricao() { return descricao; }
+
+    @Override
+    public String toString() {
+        return String.format("%s | %.2f | %s | %s", instante.toString(), valor, tipo, descricao);
+    }
 }
 
