556fb6e65699511f60269a3c5a010423e2a553f8
Aicha-Nac <nacambo.aicha@academico.ifpb.edu.br>
2025-11-21 22:23:48 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,13 +1,12 @@
-public class Cliente {
-    private String nome;
-    private String cpf;
+private List<Conta> contas = new ArrayList<>();
 
-    public Cliente(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
-    }
-
-    public String getNome() { return nome; }
-    public String getCpf() { return cpf; }
+public void adicionarConta(Conta conta) {
+    contas.add(conta);
 }
+
+public List<Conta> getContas() {
+    return contas;
+}
+
+
 
